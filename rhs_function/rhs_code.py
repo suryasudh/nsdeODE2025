e2d6ffaf2d85ec97f38f3ca68393a4620b339ac5
Aswin Kumar A <aswinkumara@iisc.ac.in>
2025-02-24 17:05:14 +0530

    gas.cp updated to gas.cv

--- rhs_function/rhs_code.py
+++ rhs_function/rhs_code.py
@@ -9,5 +9,5 @@ def rhs(t,state_in,gas): #variable rho0 assumed to be a global float
     gas.TDY = T,rho0,Y[:]
     rhs_out = np.copy(state_in)
     rhs_out[0:Neq-1] = gas.net_production_rates*gas.molecular_weights/gas.density
-    rhs_out[Neq-1] = -1.*ct.gas_constant*gas.T/(gas.cp_mass*gas.density)*np.dot(gas.net_production_rates,gas.standard_int_energies_RT)
+    rhs_out[Neq-1] = -1.*ct.gas_constant*gas.T/(gas.cv_mass*gas.density)*np.dot(gas.net_production_rates,gas.standard_int_energies_RT)
     return rhs_out
