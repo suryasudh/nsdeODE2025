87cd97d3c4f5f345991d4f33bc9805588d591509
aswinkumara <aswinkumara@iisc.ac.in>
2025-03-02 23:07:01 +0530

    new main code with adaptive dt

--- Code/main_code.py
+++ Code/main_code.py
@@ -1,8 +1,11 @@
 from timestepping import timestepper
 from rhs_code import rhs
+from new_timestep import get_dt
 import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
+dtype_global = np.float64
+dtype_time = np.float64
 
 if __name__ == '__main__':
     # Define the initial conditions
@@ -11,40 +14,57 @@ if __name__ == '__main__':
     P0 = 101325.
     X0 = "H2:2.,O2:1.,N2:3.76"
     gas_obj.TPX = T0,P0,X0
-    state_arr = np.hstack((gas_obj.Y,gas_obj.T)).ravel()
+    state_arr = np.hstack((gas_obj.Y,gas_obj.T)).ravel().astype(dtype=dtype_global)
     dens_0 = gas_obj.density_mass
+    required_methods=["rk1", "rk2"]
 
-    t=0.
-    tend=5.e-4
+
+    t = np.array(0.0, dtype=dtype_time)
+    tend=np.array(4.e-4,dtype=dtype_time)
     iter=0
-    iter_interval=100
-    dt=1.e-8
+    iter_interval=2
+    dt=np.array(1.e-8, dtype=dtype_time)
     # niters = int(tend/dt)
-    T_arr = []
     T_arr_RK1 = []
+    T_arr_RK2 = []
     YOH_arr = []
+    YH_arr = []
+    dts_arr = []
+    ts_arr = []
     while t<tend:
-        state_new = timestepper(tk=t, xk=state_arr, dt=dt, func=rhs, required_methods=["rk4", "rk1"],gas_obj=gas_obj,dens=dens_0)
-        state_arr = state_new[0, 0, :]
-        state_arr2 = state_new[1, 0, :]
+        state_new = timestepper(tk=t, xk=state_arr, dt=dt, func=rhs, required_methods=required_methods,gas_obj=gas_obj,dens=dens_0)
+        state_arr_main = state_new[0, 0, :]
+        state_arr_ref = state_new[1, 0, :]
         # print(state_new.shape)
+        state_arr = state_arr_main
 
         t+=dt
         iter=iter+1
         if iter%iter_interval==0:
             # print(f"Time={t:.4e}, Temperature={state_arr[-1]:.4f}")
-            T_arr.append(state_arr[-1])
-            T_arr_RK1.append(state_arr2[-1])
+            ts_arr.append(np.copy(t))
+            # print(f"Time={t:.4e}")
+            T_arr_RK1.append(state_arr[-1])
+            T_arr_RK2.append(state_arr_ref[-1])
             YOH_arr.append(state_arr[4])
+            YH_arr.append(state_arr[1])
         # break
         # calling the function that computes dt
-
-
-    plt.plot(T_arr,label="RK4")
-    plt.plot(T_arr_RK1,label="RK1")
+        dt = get_dt(dt_old=dt, main_method=required_methods[0], x_main=state_arr_main, x_ref=state_arr_ref, tolerance=1e-10,gamma=0.9,norm_type=2)
+        # print("dt: ",dt)
+        dts_arr.append(dt)
+    
+    plt.plot(ts_arr,T_arr_RK1,label="RK1")
+    plt.plot(ts_arr,T_arr_RK2,label="RK2")
     plt.legend()
     plt.show()
 
-    # plt.plot(YOH_arr)
-    # plt.show()
+    plt.plot(ts_arr,YOH_arr)
+    plt.show()
+
+    plt.plot(ts_arr,YH_arr)
+    plt.show()
+
+    plt.plot(dts_arr)
+    plt.show()
 
--- Code/new_timestep.py
+++ Code/new_timestep.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cantera as ct
-from typing import Callable
 
 METHOD_ORDERS: dict[str, int] = {
     "rk1": 1,
@@ -13,38 +12,18 @@ METHOD_ORDERS: dict[str, int] = {
 }
 
 def get_dt(
-    tk: float,
-    xk: np.ndarray,
     dt_old: float,
-    func: Callable[[float, np.ndarray, ct.Solution, float], np.ndarray],
     main_method: str,
-    ref_method: str,
-    gas_obj: ct.Solution,
-    dens: float,
-    tolerance: float,
+    x_main: np.ndarray,
+    x_ref: np.ndarray,
+    tolerance: float = 1e-12,
     gamma: float = 0.9,
-    norm_type: int = 2) -> tuple[float]:
-
-
-    solutions: np.ndarray = timestepper(
-        tk=tk,
-        xk=xk,
-        dt=dt_old,
-        func=func,
-        required_methods=[main_method, ref_method],
-        gas_obj=gas_obj,
-        dens=dens)
-    
-    x_main: np.ndarray = solutions[0] #Main_scheme
-    x_ref: np.ndarray = solutions[1] #reference_scheme
+    norm_type: int = 2) -> float:
 
     p_main: int = METHOD_ORDERS[main_method] # order of the main scheme
 
     error: float = np.linalg.norm(x_main[:-1] - x_ref[:-1], ord=norm_type) # computing errors for Y values alone (temperature ignored)
-    tolerance = 1e-30
-
-    if error < 1e-30:
-        error = 1e-30
+    # print("Error: ",error)
         
     dt_new: float = gamma * ((dt_old**(p_main + 1) * tolerance) / error) ** (1.0 / (p_main + 1))
     return dt_new
